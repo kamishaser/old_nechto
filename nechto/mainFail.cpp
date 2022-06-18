a03340574a07cb046d725b9d89ceb619a37ee7d3
kamishaser <91331160+kamishaser@users.noreply.github.com>
2022-06-18 18:36:02 +0300

    static library

--- nechto/mainFail.cpp
+++ nechto/mainFail.cpp
@@ -6,29 +6,3 @@
 #include "commandLine.h"
 #include "tag.h"
 #include "fileStream.h"
-
-#include <iostream>
-#include <exception>
-#include <algorithm>
-
-using namespace nechto;
-
-int main()
-{
-	commandLine c;
-	
-	while (true)
-	{
-		try
-		{
-			std::string temp;
-			std::getline(std::cin, temp);
-			std::cin.clear();
-			std::cout << c(temp) << std::endl;
-		}
-		catch (std::exception e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-	}
-}
